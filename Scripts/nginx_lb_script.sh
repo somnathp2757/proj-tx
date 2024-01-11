54c876480186975d39d3f716bb768f8f500b6560
somnath <somnathpawar20@gmail.com>
2024-01-11 12:43:39 +0530

    updated script working fine

--- Scripts/nginx_lb_script.sh
+++ Scripts/nginx_lb_script.sh
@@ -1,33 +1,47 @@
 #!/bin/bash
+#installing nginx
 apt-get update -y
 apt-get install nginx -y
 systemctl start nginx
 systemctl enable nginx
 
-#Create one file with configuration content in server at 
-#/etc/nginx/sites-available 
+#Create one file with configuration content in server at /etc/nginx/sites-available 
+rm -rf /etc/nginx/sites-enabled/default
+
 #If available default config file then delete 
 sudo ln -s /etc/nginx/sites-available/lb  /etc/nginx/sites-enabled/
 #Copy the data into file present in sites-enabled 
+echo 'upstream backend {
+    server backend-server-1;
+    server backend-server-2;
+    # Add more servers if needed
+}
+server {
+    listen 80;
+    server_name your_domain_or_server_ip;
 
-cat <<'EOF' | base64 -d > /etc/nginx/sites-available/lb
-<dXBzdHJlYW0gYmFja2VuZCB7CiAgICBzZXJ2ZXIgYmFja2VuZC1zZXJ2ZXItMTsKICAgIHNlcnZl
-ciBiYWNrZW5kLXNlcnZlci0yOwogICAgIyBBZGQgbW9yZSBzZXJ2ZXJzIGlmIG5lZWRlZAp9Cgpz
-ZXJ2ZXIgewogICAgbGlzdGVuIDgwOwogICAgc2VydmVyX25hbWUgeW91cl9kb21haW5fb3Jfc2Vy
-dmVyX2lwOwoKICAgICMgUmVkaXJlY3QgSFRUUCB0byBIVFRQUwogICAgcmV0dXJuIDMwMSBodHRw
-czovLyRob3N0JHJlcXVlc3RfdXJpOwp9CgpzZXJ2ZXIgewogICAgbGlzdGVuIDQ0MyBzc2w7CiAg
-ICBzZXJ2ZXJfbmFtZSB5b3VyX2RvbWFpbl9vcl9zZXJ2ZXJfaXA7CgogICAgc3NsX2NlcnRpZmlj
-YXRlIC9ldGMvbmdpbngvc3NsL3lvdXJfZG9tYWluLmNydDsKICAgIHNzbF9jZXJ0aWZpY2F0ZV9r
-ZXkgL2V0Yy9uZ2lueC9zc2wveW91cl9kb21haW4ua2V5OwoKICAgICMgQWRkIFNTTCBjb25maWd1
-cmF0aW9ucyBhcyBuZWVkZWQgKGUuZy4sIHNzbF9wcm90b2NvbHMsIHNzbF9jaXBoZXJzLCBldGMu
-KQoKICAgIGxvY2F0aW9uIC8gewogICAgICAgIHByb3h5X3Bhc3MgaHR0cDovL2JhY2tlbmQ7CiAg
-ICAgICAgcHJveHlfc2V0X2hlYWRlciBIb3N0ICRob3N0OwogICAgICAgIHByb3h5X3NldF9oZWFk
-ZXIgWC1SZWFsLUlQICRyZW1vdGVfYWRkcjsKICAgICAgICBwcm94eV9zZXRfaGVhZGVyIFgtRm9y
-d2FyZGVkLUZvciAkcHJveHlfYWRkX3hfZm9yd2FyZGVkX2ZvcjsKICAgICAgICBwcm94eV9zZXRf
-aGVhZGVyIFgtRm9yd2FyZGVkLVByb3RvICRzY2hlbWU7CiAgICB9Cn0KCg==
->
-EOF
+    # Redirect HTTP to HTTPS
+    return 301 https://$host$request_uri;
+}
 
+server {
+    listen 443 ssl;
+    server_name your_domain_or_server_ip;
+
+    ssl_certificate /etc/nginx/ssl/your_domain.crt;
+    ssl_certificate_key /etc/nginx/ssl/your_domain.key;
+
+    # Add SSL configurations as needed (e.g., ssl_protocols, ssl_ciphers, etc.)
+
+    location / {
+        proxy_pass http://backend;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+}
+' > /etc/nginx/sites-available/lb
 nginx -t
 systemctl reload nginx
 
